36459e7a64ac4efd29cab71be8dc4074c10af9a0
sebnemdiyar <sebnemchn@gmail.com>
2025-06-08 20:43:31 +0300

    eksikler tamamlandı

--- js/script.js
+++ js/script.js
@@ -7,39 +7,43 @@ const searchBtn = document.querySelector("#search-btn");
 const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
-searchBtn.addEventListener("click", function(){
-  searchForm.classList.toggle("active");  
-  document.addEventListener("click", function(e){
-    if(
-        !e.composedPath().includes(searchBtn) && 
-        !e.composedPath().includes(searchForm)
-    ){
-      searchForm.classList.remove("active");    
-    }
-  })
-});
+// Toggle işlemleri
+searchBtn.onclick = (e) => {
+  e.stopPropagation();
+  searchForm.classList.toggle("active");
+  cartItem.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
-cartBtn.addEventListener("click", function(){
-  cartItem.classList.toggle("active");  
-  document.addEventListener("click", function(e){
-    if(
-        !e.composedPath().includes(cartBtn) && 
-        !e.composedPath().includes(cartItem)
-    ){
-      cartItem.classList.remove("active");    
-    }
-  })
-});
+cartBtn.onclick = (e) => {
+  e.stopPropagation();
+  cartItem.classList.toggle("active");
+  searchForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
-menuBtn.addEventListener("click", function(){
-  navbar.classList.toggle("active");  
-  document.addEventListener("click", function(e){
-    if(
-        !e.composedPath().includes(menuBtn) && 
-        !e.composedPath().includes(navbar)
-    ){
-      navbar.classList.remove("active");    
-    }
-  })
-});
+menuBtn.onclick = (e) => {
+  e.stopPropagation();
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+// Sayfanın boş bir yerine tıklanınca her şeyi kapat
+document.onclick = (e) => {
+  if (!e.target.closest(".search-form") && !e.target.closest("#search-btn")) {
+    searchForm.classList.remove("active");
+  }
+
+  if (
+    !e.target.closest(".cart-items-container") &&
+    !e.target.closest("#cart-btn")
+  ) {
+    cartItem.classList.remove("active");
+  }
+
+  if (!e.target.closest(".navbar") && !e.target.closest("#menu-btn")) {
+    navbar.classList.remove("active");
+  }
+};
 
